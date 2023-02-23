aa5f07f6dca138715131a4371d1dd4b4f7226b55
sth <sth0067@gmail.com>
2023-02-23 14:50:38 -0600

    added conditional if statements for new array, created new array, reconstructed randomize function

--- Develop/script.js
+++ Develop/script.js
@@ -14,15 +14,23 @@ generateBtn.addEventListener("click", writePassword);
 //   When prompted for password criteria
 //       I select which criteria to include in the password
 
-
+var length = ""
+var lowercase = ["a","b","c","d","e","f","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numeric = ["1","2","3","4","5","6","7","8","9","0"];
+var special = ["!","@","#","$,","%","^","&","*","(",")"];
+//my new array created by confirm choices
+var possibleChar = []
+var randomPassword = ""
 
 function generatePassword () {
   passwordLength();
 //Step 3
 //   When prompted for the length of the password
 //       I choose a length of at least 8 characters and no more than 128 characters
+
 function passwordLength() {
-var length = parseInt(prompt("Enter a password length (number) between 8 and 128 characters:"));
+length = parseInt(prompt("Enter a password length (number) between 8 and 128 characters:"));
   if (length >= 8 && length <= 128) {
   chooseCharacters(); 
 } else {
@@ -38,10 +46,6 @@ var length = parseInt(prompt("Enter a password length (number) between 8 and 128
 //       I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 
 
-
-
-
-
 function chooseCharacters() {
   var selectLowercase = confirm("Include lowercase letters?");
   var selectUppercase = confirm("Include uppercase letters?");
@@ -52,9 +56,19 @@ function chooseCharacters() {
     window.alert("You must select at least one character type.");
     chooseCharacters();
   } 
-
-
-
+    if (selectLowercase) {
+      possibleChar = possibleChar.concat(lowercase);
+    }
+    if (selectUppercase) {
+      possibleChar = possibleChar.concat(uppercase);
+    }
+    if (selectNumeric) {
+      possibleChar = possibleChar.concat(numeric);
+    }
+    if (selectSpecial) {
+      possibleChar = possibleChar.concat(special);
+    }
+    
 
 
 //Step 5
@@ -62,22 +76,19 @@ function chooseCharacters() {
 //       My input should be validated and at least one character type should be selected
 
 
-
-  
-  }
-
 }
 
 
-
+}
 
 
 //Step 6
 //   When all prompts are answered 
 //      Then a password is generated that matches the selected criteria
 
-
-
+function randomize() {
+  randomPassword = possibleChar(Math.floor(Math.random()* possibleChar.length));
+}
 //Step 7
 //  When the password is generated
 //      The password is either displayed in an alert or written to the page
