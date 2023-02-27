9494bff67a391e448675ffbbf06364e216e09c1e
sth <sth0067@gmail.com>
2023-02-26 20:40:21 -0600

    cleaned up the code, removed the practice files and updated the readme file

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,41 @@
-// Assignment code here
-
-//Step 1
-// When I click the BUTTON to generate a password
-//    I am presented with a series of prompts for password criteria
-
-// Get references to the #generate element
+// References to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
+// Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Step 2
-//   When prompted for password criteria
-//       I select which criteria to include in the password
-
-var passLength = ""
+//Possible character arrays.
 var lowercase = ["a","b","c","d","e","f","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ["1","2","3","4","5","6","7","8","9","0"];
 var special = ["!","@","#","$","%","^","&","*","(",")"];
-//my new array created by confirm choices
-var possibleChar = []
-var randomPassword = []
-// var randomLowercase = ""
-// var selectLowercase = ""
-var confirmArr = [];
-var newPassLength = "";
-var guaranteeChar = [];
 
+// Global Variables.
+var possibleChar = []
+var confirmArr = [];
+var guaranteeChar = [];
+var passLength = ""
+
+
+// Main function
 function generatePassword () {
   passwordLength();
-//Step 3
-//   When prompted for the length of the password
-//       I choose a length of at least 8 characters and no more than 128 characters
 
+
+// Local function to prompt user for variable length. User will be alerted and prompted again if input is invalid.
 function passwordLength() {
-passLength = parseInt(prompt("Enter a password length (number) between 8 and 128 characters:"));
+  passLength = parseInt(prompt("Enter a password length (number) between 8 and 128 characters:"));
   if (passLength >= 8 && passLength <= 128) {
   chooseCharacters(); 
 } else {
   window.alert("Try again");
-   passwordLength()
+   passwordLength();
 }
- return; //****** */
+ 
 }
 
 
-//Step 4
-//   When asked for character types to include in the password
-//       I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-
+// Local function to prompt user which character types to include. Must include one type or user will be prompted again.
 function chooseCharacters() {
   var selectLowercase = confirm("Include lowercase letters?");
   var selectUppercase = confirm("Include uppercase letters?");
@@ -61,11 +46,13 @@ function chooseCharacters() {
     window.alert("You must select at least one character type.");
     chooseCharacters();
   } 
-  //If character type is selected, concatenate it into our custom array
+
+  //If character type is confirmed, concatenate it into a custom array.
     if (selectLowercase) {
       possibleChar = possibleChar.concat(lowercase);
 
-      //To make sure at least one character from each selection is added in the array, push a random character from the selected character type to push into the custom array
+      //To make sure at least one character from each selection is added in the array, push a random character from the 
+      //  selected character type into a custom array. "y" is just used as a counter.
       guaranteeChar.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
       confirmArr.push("y");
       
@@ -88,12 +75,11 @@ function chooseCharacters() {
       confirmArr.push("y");
 
     }
-
-
   }
+// Variable to account for the guaranteed characters.
+  var newPassLength = passLength - (confirmArr.length);
 
-  newPassLength = passLength - (confirmArr.length);
-
+  // Function to select a random item from the concatenated array.
   function selectRandomItems(array, amount) {
     var result = [];
     for (let i = 0; i < amount; i++) {
@@ -102,57 +88,20 @@ function chooseCharacters() {
     }
     return result;
   }
+
+   // Variable to call the random item function and define its parameters.
     var randomItems = selectRandomItems(possibleChar, newPassLength);
-    console.log(randomItems);
-    console.log(confirmArr);
 
-    randomPassword = guaranteeChar.concat(randomItems);
+   // Final varible that concatenates the guaranteed character array with the random items array and removes the commas within the array.
+    var randomPassword = (guaranteeChar.concat(randomItems)).join("");
 
-
-
-//Step 5
-//   When I answer each prompt
-//       My input should be validated and at least one character type should be selected
-
-
+ 
 return randomPassword;
 
 }
 
 
-
-
-
-
-//Step 6
-//   When all prompts are answered 
-//      Then a password is generated that matches the selected criteria
-
-
-
-// function writePass(possibleChar, newPassLength ) {
-//   var result = [];
-//   for ( i = 0; i < possibleChar.length; i++) {
-//    var randomChar = (Math.floor(Math.random() * newPassLength));
-//    result.push(randomChar[randomIndex]);
-//   }
-//   console.log(result)
-// }
-    // randomPassword += possibleChar.charAt(randomChar, randomChar + 1)
-//   var character = Math.floor(Math.random() * possibleChar.length);
-//   randomPassword += possibleChar.charAt(character, character + 1);
-//   console.log(randomPassword);
-
-//   }
-//   }
-
-//Step 7
-//  When the password is generated
-//      The password is either displayed in an alert or written to the page
-
-
-
-// Write password to the #password input
+// Write password function to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Password Generator Starter Code
+# Random Password Generator
+
+## Description
+
+This random password generator will prompt the user after the 'generate password' button on the webpage is 'clicked.' The first prompt will request the user to enter a length of the desired random password (within the parameters stated in the prompt). The user will then need to confirm which types of characters will be used. At least one character type must be selected or the user will be asked to confirm again. The password guarantees at least one character from each character type confirmed, and there may be duplicate characters used.
+
+
+## Installation
+
+N/A
+
+## Mock up
+
+![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+
+## Credits
+
+N/A
+
+Link to the page: https://shale00.github.io/Random-Password-Generator/
+
+## License
+
+MIT License
